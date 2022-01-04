d168cb3546f304e88df62f037273b707f9b3576c
EREZ YAKOBY <erez.yakoby@mail.huji.ac.il>
2022-01-04 20:00:25 +0200

    test PR

--- EX_5/src/pepse/PepseGameManager.java
+++ EX_5/src/pepse/PepseGameManager.java
@@ -10,7 +10,9 @@ import danogl.gui.WindowController;
 import danogl.gui.rendering.Camera;
 import danogl.util.Vector2;
 import pepse.util.CameraManager;
+import pepse.util.ScreenChunkManager;
 import pepse.world.Avatar;
+import pepse.world.Block;
 import pepse.world.Sky;
 import pepse.world.Terrain;
 import pepse.world.avatarProperties.AvatarKeymap;
@@ -22,66 +24,92 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import pepse.world.trees.Tree;
 
+/**
+ * The main class of the simulator.
+ */
 public class PepseGameManager extends GameManager {
 
-
+    private static final Color HALO_COLOR = new Color(255, 255, 0, 20);
+    private static final float AVATAR_POSITION_FACTOR = 0.2f;
     private static final float CYCLE_LENGTH = 60;
-    private static final Color haloColor = new Color(255, 255, 0, 20);
-    private static final int GROUND_SURFACE_LAYER = Layer.STATIC_OBJECTS;
-    private static final int GROUND_LAYER = Layer.STATIC_OBJECTS + 1;
-    private static final int TREES_LAYER = Layer.STATIC_OBJECTS + 1;
-    //TODO: remember to make order between the different layers
+    private static final int SEED = 3;
 
-    @Override public void initializeGame(ImageReader imageReader, SoundReader soundReader,
-                                         UserInputListener inputListener, WindowController windowController) {
+    //Layers
+    private static final int SKY_LAYER = Layer.BACKGROUND;
+    private static final int SUN_LAYER = Layer.BACKGROUND + 1;
+    private static final int TREES_LAYER = Layer.BACKGROUND + 2;
+    private static final int LEAVES_LAYER = Layer.BACKGROUND + 3;
+    private static final int SUN_HALO_LAYER = Layer.BACKGROUND + 10;
+    private static final int TERRAIN_SURFACE_LAYER = Layer.STATIC_OBJECTS;
+    private static final int TERRAIN_LAYER = Layer.STATIC_OBJECTS + 1;
+    private static final int AVATAR_LAYER = Layer.DEFAULT;
+    private static final int NIGHT_LAYER = Layer.FOREGROUND;
+
+
+    private Vector2 windowDimensions;
+    private Avatar avatar = null;
+    private Terrain terrain = null;
+    private Tree tree = null;
+    private ScreenChunkManager chunkManager;
+
+    private int curLeftX = 0;
+    private int curRightX = 0;
+
+    /**
+     * The method will be called once when a GameGUIComponent is created, and again after every
+     * invocation of windowController.resetGame().
+     *
+     * @param imageReader      - Contains a single method: readImage, which reads an image from disk.
+     *                         See its documentation for help.
+     * @param soundReader      - Contains a single method: readSound, which reads a wav file from disk.
+     *                         See its documentation for help.
+     * @param inputListener    - Contains a single method: isKeyPressed, which returns whether a given key
+     *                         is currently pressed by the user or not. See its documentation.
+     * @param windowController - Contains an array of helpful, self-explanatory methods concerning the window.
+     */
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader,
+                               UserInputListener inputListener, WindowController windowController) {
         super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        windowDimensions = windowController.getWindowDimensions();
 
-        Sky.create(
-                this.gameObjects(),
-                windowController.getWindowDimensions(),
-                Layer.BACKGROUND);
+        // Initializing Sky, Sun, Sun halo and Night
+        Sky.create(gameObjects(), windowDimensions, SKY_LAYER);
+        Night.create(gameObjects(), NIGHT_LAYER, windowDimensions, CYCLE_LENGTH);
+        GameObject sun = Sun.create(gameObjects(), SUN_LAYER, windowDimensions, CYCLE_LENGTH);
+        SunHalo.create(gameObjects(), SUN_HALO_LAYER, sun, HALO_COLOR);
 
-        Night.create(
-                this.gameObjects(),
-                Layer.FOREGROUND,
-                windowController.getWindowDimensions(),
-                CYCLE_LENGTH);
 
-        GameObject sun = Sun.create(
-                this.gameObjects(),
-                Layer.BACKGROUND + 1,
-                windowController.getWindowDimensions(),
-                CYCLE_LENGTH);
-
-        SunHalo.create(
-                this.gameObjects(),
-                Layer.BACKGROUND + 10,
-                sun,
-                haloColor);
-
-        //TODO: make seed permanent.
-        //terrain part
-        int seed = 3;
-        Vector2 windowDimensions = windowController.getWindowDimensions();
-        Terrain terrain = new Terrain(gameObjects(), GROUND_SURFACE_LAYER, windowDimensions, seed);
+        // Initializing Terrain and Trees
+        curRightX = (int) windowDimensions.x();
+        terrain = new Terrain(gameObjects(), TERRAIN_SURFACE_LAYER, windowDimensions, SEED);
         terrain.createInRange(0, (int) windowDimensions.x());
 
-        //trees part
-        Tree tree = new Tree(gameObjects(), terrain::groundHeightAt, Layer.BACKGROUND + 2, seed);
+        tree = new Tree(gameObjects(), terrain::groundHeightAt, TREES_LAYER, SEED);
         tree.createInRange(0, (int) windowDimensions.x());
 
+        gameObjects().layers().shouldLayersCollide(Layer.STATIC_OBJECTS, Layer.DEFAULT, true);
+
+        //Initializing Avatar
+        Vector2 avatarPos = windowDimensions.mult(AVATAR_POSITION_FACTOR);
+        avatar = Avatar.create(gameObjects(), AVATAR_LAYER, avatarPos, inputListener, imageReader);
+
+        setCamera(new Camera(avatar, Vector2.ZERO, windowDimensions, windowDimensions));
+
+        chunkManager = new ScreenChunkManager(gameObjects(), windowDimensions, terrain::createInRange,
+                tree::createInRange);
 
 
         //avatar creation
         //player1 creation
         Vector2 avatarLocation = windowDimensions.mult(0.2f);
+
         String player1BasePath = "EX_5/src/pepse/assets/player1-frames\\knight-frame";
         String player1suffix = ".png";
         AvatarKeymap player1Keymap = new AvatarKeymap(KeyEvent.VK_RIGHT, KeyEvent.VK_LEFT,
                 KeyEvent.VK_SPACE, KeyEvent.VK_SHIFT);
         Avatar player1 = Avatar.create(gameObjects(), Layer.DEFAULT, avatarLocation, inputListener,
                 imageReader, player1BasePath, player1suffix, player1Keymap);
-
         //player2 creation
         Vector2 player2Location = windowDimensions.mult(0.4f);
         String player2BasePath = "EX_5/src/pepse/assets/player2-frames\\player2-frame";
@@ -99,7 +127,20 @@ public class PepseGameManager extends GameManager {
         this.gameObjects().addGameObject(camManager);
     }
 
+    /**
+     * Runs the entire simulation.
+     * @param args - This argument should not be used.
+     */
     public static void main(String[] args) {
         new PepseGameManager().run();
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if (avatar != null){
+            float avatarXPosition = avatar.getCenter().x();
+            chunkManager.updateChunks(avatarXPosition);
+        }
     }
 }
--- EX_5/src/pepse/world/trees/Tree.java
+++ EX_5/src/pepse/world/trees/Tree.java
@@ -6,12 +6,17 @@ import danogl.gui.rendering.RectangleRenderable;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 import pepse.util.ColorSupplier;
+import pepse.util.ScreenChunkManager;
 import pepse.world.Block;
 
 import java.awt.*;
+import java.util.Objects;
 import java.util.Random;
 import java.util.function.Function;
 
+/**
+ * Responsible for the creation and management of trees.
+ */
 public class Tree {
     private static final Color BASIC_TREE_COLOR = new Color(100, 50, 20);
     private static final Color BASIC_LEAF_COLOR = new Color(50, 200, 30);
@@ -26,18 +31,26 @@ public class Tree {
     private static final String TREE_TAG = "tree";
     private static final String LEAF_TAG = "leaf";
 
+
     private final Function<Float, Float> groundHeightAt;
     private final GameObjectCollection gameObjects;
     private final int layer;
-    //TODO: maybe add seed
-    private final Random random;
+    private int seed;
 
+    /**
+     * CONSTRUCTOR
+     *
+     * @param gameObjects    - The collection of all participating game objects.
+     * @param groundHeightAt - reference to the method which checks the ground height.
+     * @param layer          - number of the layer for the trees.
+     * @param seed           - seed for the random object.
+     */
     public Tree(GameObjectCollection gameObjects, Function<Float, Float> groundHeightAt, int layer,
                 int seed) {
-        this.gameObjects = gameObjects;
         this.groundHeightAt = groundHeightAt;
+        this.gameObjects = gameObjects;
         this.layer = layer;
-        this.random = new Random(seed);
+        this.seed = seed;
     }
 
     /**
@@ -51,41 +64,77 @@ public class Tree {
         float curX = (float) (Math.ceil(Math.abs(minX) / blockSize) * (int) Math.signum(minX) * blockSize);
         boolean isPrevPlaced = false;
         while (curX <= maxX) {
-            if (shouldNotPlaceObj(TREE_TAG) || isPrevPlaced) {
+            Random random = new Random(Objects.hash(curX, seed));
+
+            // make sure that tree coverage percentage is correct and no side by side trees
+            if (shouldNotPlaceObj(TREE_TAG, random) || isPrevPlaced) {
                 curX += blockSize;
                 isPrevPlaced = false;
                 continue;
             }
-            int groundHeightInBlocks = (int) Math.floor(groundHeightAt.apply(curX) / blockSize);
-            int treeHeight = random.nextInt(groundHeightInBlocks - TREE_MIN_HEIGHT) + TREE_MIN_HEIGHT;
-            for (int blockIdx = 1; blockIdx < treeHeight; blockIdx++) {
-                Vector2 topLeftCorner = new Vector2(curX, (groundHeightInBlocks - blockIdx) * blockSize);
-                Renderable blockRender =
-                        new RectangleRenderable(ColorSupplier.approximateColor(BASIC_TREE_COLOR));
-                Block block = new Block(topLeftCorner, blockRender);
-                block.setTag(TREE_TAG);
-                gameObjects.addGameObject(block, layer);
-            }
-            createLeaves(new Vector2(curX, (groundHeightInBlocks - treeHeight + 1) * blockSize), gameObjects);
+            int groundHeightInBlocks = (int) (groundHeightAt.apply(curX) / blockSize);
+//            int groundHeightInBlocks = (int) Math.floor(groundHeightAt.apply(curX) / blockSize);
+            createSingleTree(gameObjects, layer, groundHeightInBlocks, curX, random);
             isPrevPlaced = true;
             curX += blockSize;
         }
-        gameObjects.layers().shouldLayersCollide(Layer.BACKGROUND + 3, Layer.STATIC_OBJECTS, true);
+
+        gameObjects.layers().shouldLayersCollide(layer + 1, Layer.DEFAULT, true);
+        gameObjects.layers().shouldLayersCollide(layer + 2, Layer.STATIC_OBJECTS, true);
     }
 
-    private boolean shouldNotPlaceObj(String objTag) {
+
+    /*
+    create a single tree.
+     */
+    private static void createSingleTree(GameObjectCollection gameObjects, int layer,
+                                         int groundHeightInBlocks, float curX, Random random) {
+        if (groundHeightInBlocks - TREE_MIN_HEIGHT <= 0) {
+            return;
+        }
+        int treeHeight = random.nextInt(groundHeightInBlocks - TREE_MIN_HEIGHT) + TREE_MIN_HEIGHT;
+        float blockSize = Block.SIZE;
+
+        for (int blockIdx = 1; blockIdx < treeHeight; blockIdx++) {
+            Vector2 topLeftCorner = new Vector2(curX, (groundHeightInBlocks - blockIdx) * blockSize);
+            Renderable blockRender =
+                    new RectangleRenderable(ColorSupplier.approximateColor(BASIC_TREE_COLOR));
+            Block block = new Block(topLeftCorner, blockRender);
+            block.setTag(TREE_TAG);
+            if (blockIdx == treeHeight - 1){
+                gameObjects.addGameObject(block, layer + 1);
+                ScreenChunkManager.addBlock(block, layer + 1);
+            }
+            else{
+                gameObjects.addGameObject(block, layer);
+                ScreenChunkManager.addBlock(block, layer);
+            }
+        }
+
+        Vector2 treeTopLeftCorner = new Vector2(curX, (groundHeightInBlocks - treeHeight + 1) * blockSize);
+        createLeaves(treeTopLeftCorner, gameObjects, random, layer + 2);
+    }
+
+    /*
+    flips an unfair coin to determine if a tree/leaf should be placed.
+     */
+    private static boolean shouldNotPlaceObj(String objTag, Random random) {
         if (objTag.equals(TREE_TAG)) {
             return random.nextInt(COIN_FLIP_BOUND) > TREE_COVER_PERCENTAGE;
         }
         return random.nextInt(COIN_FLIP_BOUND) > LEAF_COVER_PERCENTAGE;
     }
 
-    private void createLeaves(Vector2 treeTopLeftCorner, GameObjectCollection gameObjects) {
+    /*
+    creates all the leaves for a single tree.
+     */
+    private static void createLeaves(Vector2 treeTopLeftCorner, GameObjectCollection gameObjects,
+                                     Random random, int layer) {
         Vector2 leafGridTLC = treeTopLeftCorner.add(Vector2.ONES.mult(-TREE_LEAF_OFFSET_SIZE * Block.SIZE));
 
         for (int rowIdx = 0; rowIdx < TREE_LEAF_OFFSET_SIZE * 2; rowIdx++) {
             for (int colIdx = 0; colIdx < TREE_LEAF_OFFSET_SIZE * 2 + 1; colIdx++) {
-                if (shouldNotPlaceObj(LEAF_TAG)) {
+                if (shouldNotPlaceObj(LEAF_TAG, random)) {
                     continue;
                 }
                 Vector2 leafTopLeftCorner = leafGridTLC.add(new Vector2(
@@ -95,7 +144,7 @@ public class Tree {
                         new RectangleRenderable(ColorSupplier.approximateColor(BASIC_LEAF_COLOR));
 
                 Leaf leaf = new Leaf(leafTopLeftCorner, leafRenderable, random);
-                leaf.createLeaf(gameObjects, LEAF_TAG, layer + 1);
+                leaf.createLeaf(gameObjects, LEAF_TAG, layer);
             }
         }
     }
